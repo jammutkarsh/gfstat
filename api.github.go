4cd3951d5c02fe3fc2bab53bf4a3f4122afb92da
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-10-21 23:43:05 +0530

    decoupled the Access Token Retrival
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- api.github.go
+++ api.github.go
@@ -15,7 +15,10 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const port = "3639"
+const (
+	port         = "3639"
+	accessCTXKey = "access_token"
+)
 
 // Using Access Token and GitHub SDK can facilitate the use of GitHub API directly to structs.
 type BasicPageData struct {
@@ -43,17 +46,13 @@ var (
 	// Frontend
 	indexPageData = IndexPageData{githubPublicID}
 	// Context
-	background = context.Background()
+	internalGitHubCtx = context.Background()
 )
 
 func serveWebApp() {
 	fmt.Println("http://127.0.0.1:3639")
 	http.HandleFunc("/", Index)
-	http.HandleFunc("/success", GetAccessToken)
-	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
-		b := BasicPageData{}
-		Success(w, &b)
-	})
+	http.HandleFunc("/result", Result)
 	http.NotFoundHandler()
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
@@ -68,37 +67,58 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetAccessToken(w http.ResponseWriter, r *http.Request)  {
-	code := r.URL.Query().Get("code")
-	values := url.Values{"client_id": {githubPublicID}, "client_secret": {githubServerSecret}, "code": {code}, "accept": {"json"}}
+// The Result function renders the result page template and sends it as a response to the client.
+func Result(w http.ResponseWriter, r *http.Request) {
+	// I need to abstract my GitHUB OAuth2.0 API call to a function
+	// using the token, I need to display the result.
+	// But Using HTMX, I can display the result on the same page.
+	// Need to figure out what happens when I make the make the callback to the same page.
+	accessKeys := getAccessToken(w, r)
 
-	req, _ := http.NewRequest("POST", "https://github.com/login/oauth/access_token", strings.NewReader(values.Encode()))
+	client := getGitHubClient(&accessKeys.AccessToken)
+
+	user := getGitHubUser(client)
+
+	basicPageData := BasicPageData{*user, nil, nil, nil}
+
+	render := template.Must(template.New("basic.tmpl").ParseFiles("views/basic.tmpl"))
+	if err := render.Execute(w, basicPageData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// getAccessToken returns the access token from the GitHub OAuth2.0 API
+func getAccessToken(w http.ResponseWriter, r *http.Request) (creds access) {
+	sessionToken := r.URL.Query().Get("code")
+	body := url.Values{"client_id": {githubPublicID}, "client_secret": {githubServerSecret}, "code": {sessionToken}, "accept": {"json"}}
+
+	req, _ := http.NewRequest("POST", "https://github.com/login/oauth/access_token", strings.NewReader(body.Encode()))
 	req.Header.Set("Accept", "application/json")
 	resp, err := http.DefaultClient.Do(req)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return access{}
 	}
 	defer resp.Body.Close()
 
-	var access access
-
-	if err := json.NewDecoder(resp.Body).Decode(&access); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Unable to Authenticate With GitHub", http.StatusUnauthorized)
+		return access{}
 	}
-	// perform some action with the access token
 
-	// possibly redirecty to a new page /result and 
-	// render the result page template and send it as a response to the client.
-	http.Redirect(w, r, "/result", http.StatusSeeOther)
-
+	if err := json.NewDecoder(resp.Body).Decode(&creds); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return access{}
+	}
+	return creds
 }
 
 // Authenticates GitHub Client with provided OAuth access token
+// this client allows us to make make changes directly to the user's GitHub account
+// without needing to manually enter various URLs and tokens
 func getGitHubClient(accessToken *string) *github.Client {
-	ctx := background
+	ctx := internalGitHubCtx
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: *accessToken},
 	)
@@ -106,8 +126,9 @@ func getGitHubClient(accessToken *string) *github.Client {
 	return github.NewClient(tc)
 }
 
+// getGitHubUser returns the GitHub user associated with the provided GitHub client
 func getGitHubUser(client *github.Client) *github.User {
-	user, _, err := client.Users.Get(background, "")
+	user, _, err := client.Users.Get(internalGitHubCtx, "")
 	if err != nil {
 		log.Println(err)
 		return nil
