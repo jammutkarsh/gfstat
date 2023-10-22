4174a43bf3e1123db3f42a72ec55841fdd1f1d88
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-10-22 10:13:16 +0530

    test: updated tests with file IO
    Prev: the tests were implemented using a object oriented method. New: But the codebase has changed from custom structs to GitHub's predefined struct. This has allowed me to make code more robust. Now there are only two external structs: github SDK and x/OAuth. Everything else is from std lib.
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- api.profile.go
+++ api.profile.go
@@ -13,40 +13,19 @@ type MetaFollow struct {
 	HTMLURL  string `json:"html_url"`
 }
 
-// GoProfile is more abstracted version of the UserProfile struct.
-// It holds the followers and following raw data, which could be processed immediately
-// instead of having to make multiple API calls.
-// This struct follows created for the same reason as UserProfile,
-// to simplify the data structure and is subject to change based on needs.
-type CurrentUser struct {
-	Username       string       `json:"username"`
-	Type           string       `json:"type"` // User or Organization
-	HTMLURL        string       `json:"html_url"`
-	Followers      []MetaFollow `json:"followers"`
-	FollowersCount int
-	Following      []MetaFollow `json:"following"`
-	FollowingCount int
-}
-
-func (c *CurrentUser) setMetadata(username, htmlURL, accountType string) {
-	c.Username = username
-	c.HTMLURL = htmlURL
-	c.Type = accountType
-}
-
 // Mutuals gives the list of mutuals between followers and following.
 // TC: O(nLogn)
 // SC: O(1)
-func (c CurrentUser) Mutuals() []MetaFollow {
+func Mutuals(followers, following []MetaFollow) []MetaFollow {
 	var mutuals []MetaFollow
-	for _, follower := range c.Followers {
-		low, high := 0, len(c.Following)-1
+	for _, follower := range followers {
+		low, high := 0, len(following)-1
 		for low <= high {
 			mid := low + (high-low)/2
-			if follower.Username == c.Following[mid].Username {
+			if follower.Username == following[mid].Username {
 				mutuals = append(mutuals, follower)
 				break
-			} else if follower.Username < c.Following[mid].Username {
+			} else if follower.Username < following[mid].Username {
 				high = mid - 1
 			} else {
 				low = mid + 1
@@ -59,14 +38,14 @@ func (c CurrentUser) Mutuals() []MetaFollow {
 // followers - following
 // TC: O(n)
 // SC: O(N)
-func (c CurrentUser) FollowersYouDontFollow() []MetaFollow {
+func FollowersYouDontFollow(followers, following []MetaFollow) []MetaFollow {
 	m := make(map[string]MetaFollow)
-	for _, following := range c.Following {
+	for _, following := range following {
 		m[following.Username] = following
 	}
 
 	var iDontFollow []MetaFollow
-	for _, follower := range c.Followers {
+	for _, follower := range followers {
 		if _, ok := m[follower.Username]; !ok {
 			iDontFollow = append(iDontFollow, follower)
 		}
@@ -77,16 +56,16 @@ func (c CurrentUser) FollowersYouDontFollow() []MetaFollow {
 // following - followers
 // TC: O(n)
 // SC: O(N)
-func (c CurrentUser) FollowingYouDontFollow() []MetaFollow {
+func FollowingYouDontFollow(followers, following []MetaFollow) []MetaFollow {
 	m := make(map[string]MetaFollow)
-	for _, follower := range c.Followers {
+	for _, follower := range followers {
 		if _, ok := m[follower.Username]; !ok {
 			m[follower.Username] = follower
 		}
 	}
 
 	var iDontFollow []MetaFollow
-	for _, following := range c.Following {
+	for _, following := range following {
 		if _, ok := m[following.Username]; !ok {
 			iDontFollow = append(iDontFollow, following)
 		}
--- api.profile_test.go
+++ api.profile_test.go
@@ -9,84 +9,72 @@ import (
 
 const testInput = "./testData/test.input.json"
 
-var input CurrentUser
+var input struct {
+	Username  string `json:"username"`
+	Followers []MetaFollow `json:"followers"`
+	Following []MetaFollow `json:"following"`
+}
+
+type testFields struct {
+	Followers []MetaFollow
+	Following []MetaFollow
+}
 
 func init() {
 	testDataReader(&input, testInput)
 }
 
-type fields struct {
-	Username  string
-	Followers []MetaFollow
-	Following []MetaFollow
-}
-
 func testDataReader(v any, file string) {
 	bytes, err := os.ReadFile(file)
 	if err != nil {
-		panic(err)
+		panic("error in reading test file:" + err.Error() + "\n")
 	}
 	if err := json.Unmarshal(bytes, v); err != nil {
-		panic(err)
+		panic("error in converting test file to JSON:" + err.Error() + "\n")
 	}
 }
 
-func TestCurrentUser_Mutuals(t *testing.T) {
+func TestMutuals(t *testing.T) {
 	t.Parallel()
 	var want []MetaFollow
 	testDataReader(&want, "./testData/test.output.mutuals.json")
-	f := fields{
-		Username:  input.Username,
+	f := testFields{
 		Followers: input.Followers,
 		Following: input.Following,
 	}
 	t.Run("Test 1", func(t *testing.T) {
-		c := CurrentUser{
-			Followers: f.Followers,
-			Following: f.Following,
-		}
-		if got := c.Mutuals(); !reflect.DeepEqual(got, want) {
-			t.Errorf("\nCurrentUser.Mutuals() = %v\nwant %v", got, want)
+		if got := Mutuals(f.Followers, f.Following); !reflect.DeepEqual(got, want) {
+			t.Errorf("\nMutuals() = %v\nwant %v", got, want)
 		}
 	})
 }
 
-func TestCurrentUser_FollowersYouDontFollow(t *testing.T) {
+func TestFollowersYouDontFollow(t *testing.T) {
 	t.Parallel()
 	var want []MetaFollow
 	testDataReader(&want, "./testData/test.output.iDontFollow.json")
-	f := fields{
-		Username:  input.Username,
+	f := testFields{
 		Followers: input.Followers,
 		Following: input.Following,
 	}
 	t.Run("Test 1", func(t *testing.T) {
-		c := CurrentUser{
-			Followers: f.Followers,
-			Following: f.Following,
-		}
-		if got := c.FollowersYouDontFollow(); !reflect.DeepEqual(got, want) {
-			t.Errorf("\nCurrentUser.FollowersYouDontFollow() = %v\nwant %v", got, want)
+		if got := FollowersYouDontFollow(f.Followers, f.Following); !reflect.DeepEqual(got, want) {
+			t.Errorf("\nFollowersYouDontFollow() = %v\nwant %v", got, want)
 		}
 	})
 }
 
-func TestCurrentUser_FollowingYouDontFollow(t *testing.T) {
+func TestFollowingYouDontFollow(t *testing.T) {
 	t.Parallel()
 	var want []MetaFollow
 	testDataReader(&want, "./testData/test.output.theyDontFollow.json")
-	f := fields{
-		Username:  input.Username,
+	f := testFields{
 		Followers: input.Followers,
 		Following: input.Following,
 	}
 	t.Run("Test 1", func(t *testing.T) {
-		c := CurrentUser{
-			Followers: f.Followers,
-			Following: f.Following,
-		}
-		if got := c.FollowingYouDontFollow(); !reflect.DeepEqual(got, want) {
-			t.Errorf("\nCurrentUser.FollowingYouDontFollow() = %v\nwant %v", got, want)
+		if got := FollowingYouDontFollow(f.Followers, f.Following); !reflect.DeepEqual(got, want) {
+			t.Errorf("\nFollowingYouDontFollow() = %v\nwant %v", got, want)
 		}
 	})
 }
