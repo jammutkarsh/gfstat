78937632dfd2645165ee6ac516f72c67a1ed1e3b
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2024-01-09 03:17:49 +0530

    pref: making concurrent API calls to GitHub
    This increased the speed by almost 2x in my local testing
    
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- services/web/web.go
+++ services/web/web.go
@@ -11,12 +11,7 @@ import (
 	"github.com/google/go-github/github"
 )
 
-const port = "3639"
-
-var indexPageData = IndexPageData{client.GithubPublicID}
-
-// Using Access Token and GitHub SDK can facilitate the use of GitHub API directly to structs.
-type ResultPageData struct {
+type ResultData struct {
 	ClientID       string
 	User           github.User
 	Mutuals        []core.MetaFollow
@@ -24,14 +19,14 @@ type ResultPageData struct {
 	TheyDontFollow []core.MetaFollow
 }
 
-// IndexPageData is the data for the index page template
-type IndexPageData struct {
+// IndexData is the data for the index page template
+type IndexData struct {
 	ClientID string
 }
 
-type UnknownError struct {
-	Err string
-}
+const port = "3639"
+
+var indexPageData = IndexData{client.GithubPublicID}
 
 func ServeWebApp() {
 	log.Println("Serving Web App on port: ", port)
@@ -42,7 +37,6 @@ func ServeWebApp() {
 	}
 }
 
-// The index function renders the index page template and sends it as a response to the client.
 func index(w http.ResponseWriter, r *http.Request) {
 	indexPage := template.Must(template.New("index.html").ParseFiles("./views/index.html"))
 	if err := indexPage.Execute(w, indexPageData); err != nil {
@@ -52,19 +46,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func renderErrorPage(w http.ResponseWriter, err error) {
 	render := template.Must(template.New("error.html").ParseFiles("./views/error.html"))
-	htmlErr := UnknownError{err.Error()}
-	if err := render.Execute(w, htmlErr); err != nil {
+	if err := render.Execute(w, err.Error()); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	log.Println("Error: ", err)
 }
 
-// The result function renders the result page template and sends it as a response to the client.
 func result(w http.ResponseWriter, r *http.Request) {
-	// I need to abstract my GitHUB OAuth2.0 API call to a function
-	// using the token, I need to display the result.
-	// But Using HTMX, I can display the result on the same page.
-	// Need to figure out what happens when I make the make the callback to the same page.
 	if !r.URL.Query().Has("code") {
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
@@ -79,41 +67,45 @@ func result(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	ghClient := client.GetGitHubClient(&accessKeys.AccessToken)
-	ghUser := client.GetGitHubUser(ghClient)
+	ghUser, ghResp := client.GetGitHubUser(ghClient)
 
 	// Get the followers of the user
-	followers, err := client.GETFollowers(ghClient, *ghUser)
+	var (
+		followers []core.MetaFollow
+		following []core.MetaFollow
+		wg        sync.WaitGroup
+		err       error
+	)
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		followers, err = client.GETFollowers(ghClient, *ghUser, *ghResp)
+	}()
+	go func() {
+		defer wg.Done()
+		following, err = client.GETFollowing(ghClient, *ghUser, *ghResp)
+	}()
+	wg.Wait()
+
 	if err != nil {
 		renderErrorPage(w, err)
 		return
 	}
 
-	// Get the following of the user
-	following, err := client.GETFollowing(ghClient, *ghUser)
-	if err != nil {
-		renderErrorPage(w, err)
-		return
-	}
-	/* The increased capacity of channel avoids deadlock for the c variable.
-	The 3 go routines can run in concurrently without blocking each other.
-	*/
-	// results, resultsCh := make([][]MetaFollow, 3), make([]chan []MetaFollow, 3)
 	resultsCh := make([]chan []core.MetaFollow, 3)
 	for i := range resultsCh {
 		resultsCh[i] = make(chan []core.MetaFollow, 1)
 	}
-	var wg sync.WaitGroup
 	wg.Add(3)
 	go core.Mutuals(followers, following, resultsCh[0], &wg)
 	go core.IDontFollow(followers, following, resultsCh[1], &wg)
 	go core.TheyDontFollow(followers, following, resultsCh[2], &wg)
 	wg.Wait()
-	close(resultsCh[0])
-	close(resultsCh[1])
-	close(resultsCh[2])
-	basicPageData := ResultPageData{client.GithubPublicID, *ghUser, <-resultsCh[0], <-resultsCh[1], <-resultsCh[2]}
+
+	basicPageData := ResultData{client.GithubPublicID, *ghUser, <-resultsCh[0], <-resultsCh[1], <-resultsCh[2]}
 	render := template.Must(template.New("basic.html").ParseFiles("./views/basic.html"))
-	if err := render.Execute(w, basicPageData); err != nil {
+	if err = render.Execute(w, basicPageData); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
